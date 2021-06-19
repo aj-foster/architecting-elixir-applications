cd1212d70db5b88ba6ce0e96df585ea55ad871e4
AJ Foster <web@aj-foster.com>
2021-06-19 15:26:49 -0400

    Allow supporting modules in test/support

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Globolive.MixProject do
       app: :globolive,
       version: "0.1.0",
       elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -16,6 +17,9 @@ defmodule Globolive.MixProject do
       extra_applications: [:logger]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     []
