51db6cb55fc68b7b75c7eae136023f7273925913
AJ Foster <web@aj-foster.com>
2021-07-12 23:45:59 -0400

    Use start_supervised!/1 to start event manager processes
    This is safer than simply calling start_link/1, because
    it guarantees the process will be stopped before the next test.

--- test/globolive/boundary/event_manager_ets_test.exs
+++ test/globolive/boundary/event_manager_ets_test.exs
@@ -7,13 +7,13 @@ defmodule Globolive.Boundary.EventManagerETSTest do
 
   describe "start_link/1" do
     test "starts the manager with no events and a default name" do
-      assert {:ok, _pid} = EventManagerETS.start_link()
+      assert {:ok, _pid} = start_supervised(EventManagerETS)
       refute is_nil(GenServer.whereis(EventManagerETS))
     end
 
     test "starts the manager with pre-hydrated events" do
       events = [event_with_attraction(name: "Event1"), event_with_attraction(name: "Event2")]
-      assert {:ok, _pid} = EventManagerETS.start_link(events: events)
+      assert {:ok, _pid} = start_supervised({EventManagerETS, [events: events]})
       assert %Event{} = EventManagerETS.get_event_by_name("Event1")
       assert %Event{} = EventManagerETS.get_event_by_name("Event2")
     end
@@ -22,7 +22,7 @@ defmodule Globolive.Boundary.EventManagerETSTest do
   describe "add_event/2" do
     test "adds an event to the manager" do
       event = event_fields(name: "Event1")
-      EventManagerETS.start_link()
+      start_supervised!(EventManagerETS)
       assert :ok = EventManagerETS.add_event(event)
       assert %Event{} = EventManagerETS.get_event_by_name("Event1")
     end
@@ -31,13 +31,13 @@ defmodule Globolive.Boundary.EventManagerETSTest do
   describe "add_attraction_event/3" do
     test "adds an attraction to an existing event" do
       events = [event_with_attraction(name: "Event1")]
-      EventManagerETS.start_link(events: events)
+      start_supervised!({EventManagerETS, [events: events]})
       assert :ok = EventManagerETS.add_attraction_to_event("Event1", attraction_fields())
       assert %Event{attraction_count: 2} = EventManagerETS.get_event_by_name("Event1")
     end
 
     test "returns error for a non-existent event" do
-      EventManagerETS.start_link()
+      start_supervised!(EventManagerETS)
       assert :error = EventManagerETS.add_attraction_to_event("Non-existent", attraction_fields())
     end
   end
@@ -45,13 +45,13 @@ defmodule Globolive.Boundary.EventManagerETSTest do
   describe "get_event_by_name/2" do
     test "returns an event by its name" do
       events = [event_with_attraction(name: "Event1"), event_with_attraction(name: "Event2")]
-      EventManagerETS.start_link(events: events)
+      start_supervised!({EventManagerETS, [events: events]})
       assert %Event{name: "Event1"} = EventManagerETS.get_event_by_name("Event1")
     end
 
     test "returns nothing for a non-existent event" do
       events = [event_with_attraction(name: "Event1"), event_with_attraction(name: "Event2")]
-      EventManagerETS.start_link(events: events)
+      start_supervised!({EventManagerETS, [events: events]})
       assert is_nil(EventManagerETS.get_event_by_name("Non-existent"))
     end
   end
--- test/globolive/boundary/event_manager_test.exs
+++ test/globolive/boundary/event_manager_test.exs
@@ -7,18 +7,18 @@ defmodule Globolive.Boundary.EventManagerTest do
 
   describe "start_link/1" do
     test "starts the manager with no events and a default name" do
-      assert {:ok, _pid} = EventManager.start_link()
+      assert {:ok, _pid} = start_supervised(EventManager)
       refute is_nil(GenServer.whereis(EventManager))
     end
 
     test "starts the manager with a custom name" do
-      assert {:ok, _pid} = EventManager.start_link(name: Globolive.Test.EventManager)
+      assert {:ok, _pid} = start_supervised({EventManager, [name: Globolive.Test.EventManager]})
       refute is_nil(GenServer.whereis(Globolive.Test.EventManager))
     end
 
     test "starts the manager with pre-hydrated events" do
       events = [event_with_attraction(name: "Event1"), event_with_attraction(name: "Event2")]
-      assert {:ok, _pid} = EventManager.start_link(events: events)
+      assert {:ok, _pid} = start_supervised({EventManager, [events: events]})
       assert %Event{} = EventManager.get_event_by_name("Event1")
       assert %Event{} = EventManager.get_event_by_name("Event2")
     end
@@ -27,7 +27,7 @@ defmodule Globolive.Boundary.EventManagerTest do
   describe "add_event/2" do
     test "adds an event to the manager" do
       event = event_fields(name: "Event1")
-      EventManager.start_link()
+      start_supervised!(EventManager)
       assert :ok = EventManager.add_event(event)
       assert %Event{} = EventManager.get_event_by_name("Event1")
     end
@@ -36,13 +36,13 @@ defmodule Globolive.Boundary.EventManagerTest do
   describe "add_attraction_event/3" do
     test "adds an attraction to an existing event" do
       events = [event_with_attraction(name: "Event1")]
-      EventManager.start_link(events: events)
+      start_supervised!({EventManager, [events: events]})
       assert :ok = EventManager.add_attraction_to_event("Event1", attraction_fields())
       assert %Event{attraction_count: 2} = EventManager.get_event_by_name("Event1")
     end
 
     test "returns error for a non-existent event" do
-      EventManager.start_link()
+      start_supervised!(EventManager)
       assert :error = EventManager.add_attraction_to_event("Non-existent", attraction_fields())
     end
   end
@@ -50,13 +50,13 @@ defmodule Globolive.Boundary.EventManagerTest do
   describe "get_event_by_name/2" do
     test "returns an event by its name" do
       events = [event_with_attraction(name: "Event1"), event_with_attraction(name: "Event2")]
-      EventManager.start_link(events: events)
+      start_supervised!({EventManager, [events: events]})
       assert %Event{name: "Event1"} = EventManager.get_event_by_name("Event1")
     end
 
     test "returns nothing for a non-existent event" do
       events = [event_with_attraction(name: "Event1"), event_with_attraction(name: "Event2")]
-      EventManager.start_link(events: events)
+      start_supervised!({EventManager, [events: events]})
       assert is_nil(EventManager.get_event_by_name("Non-existent"))
     end
   end
